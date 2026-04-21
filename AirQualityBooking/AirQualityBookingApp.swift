e1d0019bcb1b38878d1d9127e74009e6cfeb29fc
Kumar praveen <kupraveen1@gmail.com>
2026-04-21 16:46:24 +0530

    Initial Commit

---
+++ AirQualityBooking/AirQualityBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityBookingApp.swift
+//  AirQualityBooking
+//
+//  Created by Praveen on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
